ae7d5a61672fcc07a867e0ce9f9ac63d4d4a12c8
Cadence-Studio <cadence@cadencestudio.co.nz>
2025-12-15 09:49:44 +0000

    test

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 "use strict";
 
 var SPOTIFY_CLIENT_ID = '73ab9fad9b594bfb9e5e63e9b7822358';
-var SPOTIFY_REDIRECT_URI = 'https://Cadence-Studio.github.io/SortYourMusic/';
+var SPOTIFY_REDIRECT_URI = 'https://cadence-studio.github.io/SortYourMusic/';
